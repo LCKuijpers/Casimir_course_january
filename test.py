925432d819e8eb28df957a11d909697f09cedb4f
louis <L.C.Kuijpers@tudelft.nl>
2021-01-25 15:13:38 +0100

    add docstring

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 r = 2
+
+"calculation of the circumvent of the circle"
 def cv(r):
     return 2 * r *np.pi
 
 print('the circumvent of the circle is', cv(r))
 
+
+"calculation of the surface area of the circle"
 def surf(r):
     return 2 ** r *np.pi
 
